aff59e6b5103a3c31e94ef1632e93ccb18be4f97
AntonioValentin <cesar_anto30@hotmail.com>
2024-01-31 22:54:02 -0600

    Implementacion del PDF

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1100,
+    height: 1000,
     webPreferences: {
       nodeIntegration: true,
     },
